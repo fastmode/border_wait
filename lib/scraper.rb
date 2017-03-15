1f4737527158cc1aa8114f1dea7a925131c8e47c
Art Rodriguez <art.rodriguez@live.com>
2017-03-15 16:50:51 -0400

    add commercial vehicle lanes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ doc = Nokogiri::HTML(open(site))
 ports = []
 
 doc.css("port").each do |p|
+	# Port Information
 	port_number = p.css("port_number").text
 	border = p.css("border").text
 	port_name = p.css("port_name").text
@@ -16,6 +17,12 @@ doc.css("port").each do |p|
 	date = p.css("date").text
 	port_status = p.css("port_status").text
 
+	# Commercial Vehicle Lanes Information
+	comm_max_lanes = p.css("commercial_vehicle_lanes maximum_lanes").text
+	comm_standard_update_time = p.css("commercial_vehicle_lanes standard_lanes update_time").text
+	comm_standard_operational_status = p.css("commercial_vehicle_lanes standard_lanes operational_status").text
+	comm_standard_delay_minutes = p.css("commercial_vehicle_lanes standard_lanes delay_minutes").text
+	comm_standard_lanes_open = p.css("commercial_vehicle_lanes standard_lanes lanes_open").text
 
-	#p.css("commercial_vehicle_lanes standard_lanes lanes_open").text
+	binding.pry
 end
