d64ea058bdab5149aa2af66f1b7c38fb3ca8c1e5
Art Rodriguez <art.rodriguez@live.com>
2017-03-15 16:59:12 -0400

    completed commercial vehicle lanes info

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,11 @@ doc.css("port").each do |p|
 	comm_standard_operational_status = p.css("commercial_vehicle_lanes standard_lanes operational_status").text
 	comm_standard_delay_minutes = p.css("commercial_vehicle_lanes standard_lanes delay_minutes").text
 	comm_standard_lanes_open = p.css("commercial_vehicle_lanes standard_lanes lanes_open").text
+	comm_fast_operational_status = p.css("commercial_vehicle_lanes fast_lanes operational_status").text
+	comm_fast_update_time = p.css("commercial_vehicle_lanes fast_lanes update_time").text
+	comm_fast_delay_minutes = p.css("commercial_vehicle_lanes fast_lanes delay_minutes").text
+	comm_fast_lanes_open = p.css("commercial_vehicle_lanes fast_lanes lanes_open").text
 
+	
 	binding.pry
 end
