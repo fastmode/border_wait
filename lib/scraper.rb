92764e53262fb4e6607c3252945b6ddbc8891d73
Art Rodriguez <art.rodriguez@live.com>
2017-03-15 16:14:09 -0400

    add spec.md

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,1 +1,9 @@
-#This will scrape data
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+site = "https://apps.cbp.gov/bwt/bwt.xml"
+doc = Nokogiri::HTML(open(site))
+#doc = Nokogiri::XML(open(site))
+
